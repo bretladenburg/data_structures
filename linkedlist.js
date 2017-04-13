3f4e69c7be95ab2aa73102f146c907a12db6a307
Bret Ldenburg <bret.ladenburg@gmail.com>
2017-04-13 16:58:45 -0700

    worked on the second part of the data structures assignment

--- linkedlist.js
+++ linkedlist.js
@@ -7,21 +7,32 @@ function Node(value, next) {
 
 function LinkedList() {
   this.head = null;
+  this.length = 0;
 }
 
 LinkedList.prototype.push = function(value) {
   this.head = new Node(value, this.head);
+  this.length++;
 };
 LinkedList.prototype.display = function() {
+  var curr = this.head;
+  var output = '';
+  while(curr) {
+    output += curr.value + '->';
+    curr = curr.next;
+  }
+  return output;
+
+  var list = new LinkedList();
   //visualize whatever data is in your linked list as a string.
   //ex: [5] -> [6] -> [3] -> [18]
 
   /*
-  war list = new LinkedList();
+  var list = new LinkedList();
   list.push(18);
   list.push(3);
   list.push(6);
   list.push(5);
-  list.display
+  list.display() returns '[5]'-> '[6]'-> '[3]'
   */
 };
