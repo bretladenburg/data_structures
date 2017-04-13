aba97bd29ac281074f8c4d900af8ac61662e26a6
Bret Ldenburg <bret.ladenburg@gmail.com>
2017-04-13 14:46:17 -0700

    added the instructions for new data structures lesson

--- linkedlist.js
+++ linkedlist.js
@@ -10,6 +10,18 @@ function LinkedList() {
 }
 
 LinkedList.prototype.push = function(value) {
-  var unicorn = new Node(value, this.head);
-  this.head = unicorn;
+  this.head = new Node(value, this.head);
+};
+LinkedList.prototype.display = function() {
+  //visualize whatever data is in your linked list as a string.
+  //ex: [5] -> [6] -> [3] -> [18]
+
+  /*
+  war list = new LinkedList();
+  list.push(18);
+  list.push(3);
+  list.push(6);
+  list.push(5);
+  list.display
+  */
 };
